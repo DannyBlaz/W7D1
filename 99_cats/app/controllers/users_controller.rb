4e6c3599782791edb744cfb432cfaab8dfa5c136
Daniel <dannyblaze469@gmail.com>
2021-04-05 15:13:00 -0400

    Created routes and Created methods for users controller

---
+++ 99_cats/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_url(@user.id)
+        end
+    end
+
+
+end
